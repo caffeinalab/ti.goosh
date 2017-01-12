ba263e27a824a6d51d12908378455f69cec868b7
Flavio De Stefano <flavio.destefano@caffeina.com>
2017-01-12 13:29:22 +0100

    Recompilation with 64bit

--- example/app.js
+++ example/app.js
@@ -1,20 +1,15 @@
 var TiGoosh = require('ti.goosh');
 TiGoosh.registerForPushNotifications({
 
-
     // The callback to invoke when a notification arrives.
     callback: function(e) {
-
         var data = JSON.parse(e.data || '');
-
     },
 
     // The callback invoked when you have the device token.
     success: function(e) {
-
         // Send the e.deviceToken variable to your PUSH server
         Ti.API.log('Notifications: device token is ' + e.deviceToken);
-
     },
 
     // The callback invoked on some errors.
